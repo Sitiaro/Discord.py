c42e34f0e862d196d3d47597678b5adf7ea23b8f
Sitiaro <46511306+Sitiaro@users.noreply.github.com>
2021-09-11 19:08:26 +0300

    Update discord.py

--- discord.py
+++ discord.py
@@ -11,4 +11,4 @@ for i in range(len(cogs)): #to activate the cogs
     cogs[i].setup(client)
     print('Setup successful.')
     
-client.run('token here')
+client.run('token here') #the discord bot token from developer portal goes here
