d99743b678854ed07946deb6a9259bb4b0289e8f
Sitiaro <46511306+Sitiaro@users.noreply.github.com>
2022-05-03 11:33:00 +0300

    Update levels.py

--- levels.py
+++ levels.py
@@ -3,6 +3,7 @@ import discord
 from discord.ext import commands
 from pymongo import MongoClient
 import asyncio
+import youtube_dl
  
 bot_channel = #put the channel ID here. You can get the channel ID by heading over to the channel > right click > copy channel ID
 talk_channels = [] #ID's of every channel wherein you want the bot to allot xp to a user
@@ -102,6 +103,51 @@ class levelsys(commands.Cog):
                 if i == 11:
                     break
             await ctx.channel.send(embed=embed)
- #setting up cogs
+
+#music commands
+#you'll need a youtube link for music commands. Queuing music will be added soon! :')
+class music(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def join(self, ctx):
+        if ctx.author.voice is None:
+            await ctx.send("Please join a voice channel.")
+        voice_channel = ctx.author.voice.channel
+        if ctx.voice_client is None:
+            await voice_channel.connect()
+        else:
+            await ctx.voice_client.move_to(voice_channel)
+
+    @commands.command()
+    async def disconnect(self, ctx):
+        await ctx.voice_client.disconnect()
+
+
+    @commands.command()
+    async def play(self, ctx, url):
+        ctx.voice_client.stop()
+        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+        YDL_OPTIONS = {'format': 'bestaudio'}
+        vc = ctx.voice_client
+
+        with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(url, download=False)
+            url2 = info['formats'][0]['url']
+            source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
+            vc.play(source)
+
+    @commands.command()
+    async def pause(self, ctx):
+        await ctx.voice_client.pause()
+        await ctx.send('Paused.')
+
+    @commands.command()
+    async def pause(self, ctx):
+        await ctx.voice_client.pause()
+        await ctx.send('Resumed.')
+            
+#setting up cogs
 def setup(client):
     client.add_cog(levels(client))
